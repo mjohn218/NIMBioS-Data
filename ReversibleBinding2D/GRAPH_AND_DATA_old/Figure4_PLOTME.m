411513b20e43eb036032ad25eb1c8a26491bf164
Athena Chen <achen70@jhu.edu>
2016-10-12 23:23:47 -0400

    Added 2D Irreversible graphs
    -FPR looks off for 1um^2.s-1; no previous FPR data for 10 um^2.s-1
    -Updated VCell (correct SA values, diffusion coefficients, and initial
    conditions
    -Smoldyn simulations run; data too large to access

---
+++ ReversibleBinding2D/GRAPH_AND_DATA_old/Figure4_PLOTME.m
@@ -0,0 +1,211 @@
+%% Figure 1 - Simple Reversible Reaction 2D Plots
+% Clear
+close all
+clear
+clc
+
+%% Import
+% t is in us
+load('time.mat');
+load('det1D.mat');
+load('det3D.mat');
+load('gillespie.mat');
+load('mcell.mat');
+load('smoldyn.mat');
+load('fpr.mat');
+
+load('det1DBC.mat');
+load('det3DBC.mat');
+load('gillespieBC.mat');
+load('mcellBC.mat');
+load('smoldynBC.mat');
+load('fprBC.mat');
+
+%% Process data
+%   Data files containing one or more trials are averaged
+%   Data points corresponding to the time vector are extrapolated
+%   Time is converted to us
+
+% Theory
+%   time: us
+initA = 1e-2; %initial concentration in nm^-3
+sigma = 1; %nm
+D = 100; %nm^2.um-1
+kD = 4*pi*sigma*D;
+ka = 1000; %nm^3.um-1
+lambda = (D/sigma^2)*(1+(ka/kD))^2;
+theory = ((1/initA)-kD*(ka/(ka+kD))*(ka/(kD*lambda))+kD*(ka/(ka+kD)).*...
+    (time+((ka.*2.*sqrt(time))./(kD.*sqrt(lambda*pi)))+...
+    (ka/(kD*lambda)).*erfcx(sqrt(lambda*time)))).^(-1);
+theory = theory*2e5; %convert to number of molecules
+
+% ODE 
+%   time: s
+det1D = interp1(det1D(:,1)*1e6, det1D(:,2),time,'pchip',0);
+
+% PDE
+%   2 trials
+%   time: s
+det3D(:,2) = (det3D(:,2) + det3D(:,4))/2;
+det3D = interp1(det3D(:,1)*1e6,det3D(:,2), time,'pchip', 0);
+
+% Gillespie
+%   20 trials
+%   time: s
+temp = 0;
+for n = 1:2:20
+    temp = temp(:,1) + interp1(gillespie(:,n)*1e6,gillespie(:,n+1), time, 'pchip',0);
+end
+gillespie = temp(:,1)/10;
+clear temp
+
+% MCell
+%   time: s
+mcell = interp1(mcell(:,1)*1e6, mcell(:,2), time, 'pchip', 0);
+
+% Smoldyn
+%   10 trials
+%   time: s
+for n = 3:11
+    smoldyn(:,2) = smoldyn(:,2)+smoldyn(:,n);
+end
+smoldyn(:,2) = smoldyn(:,2)./10;
+smoldyn = interp1(smoldyn(:,1)*1e6, smoldyn(:,2), time, 'pchip', 0);
+
+% FPR
+%   Deletes first row
+%   10 trials; already averaged
+%   time: us
+fpr = interp1(fpr(2:size(fpr),1),fpr(2:size(fpr),2),time,'pchip',0);
+
+% TheoryBC
+%   time: us
+theoryBC = ((1/initA)+2*kD.*(time+(2*sigma.*sqrt(time))./(sqrt(2*D*pi)))).^(-1);
+theoryBC = theoryBC*2e5;
+
+% ODEBC
+%   time: s
+det1DBC = interp1(det1DBC(:,1)*1e6,det1DBC(:,2), time, 'pchip',0);
+
+% PDEBC
+%   2 trials
+%   time: s
+det3DBC(:,2) = (det3DBC(:,2) + det3DBC(:,4))/2;
+det3DBC = interp1(det3DBC(:,1)*1e6,det3DBC(:,2), time, 'pchip', 0);
+
+% GillespieBC
+%   20 trials
+%   time: s
+temp = 0;
+for n = 1:2:20
+    temp = temp(:,1) + interp1(gillespieBC(:,n)*1e6,gillespieBC(:,n+1), time, 'pchip',0);
+end
+gillespieBC = temp(:,1)/10;
+clear temp
+
+% MCellBC
+%   time: s
+
+% SmoldynBC
+%   10 trials
+%   time: s
+smoldynBC = interp1(smoldynBC(:,1)*1e6, smoldynBC(:,2), time, 'pchip', 0);
+
+% FPRBC
+%   Deletes first row
+%   10 trials, already averaged
+%   time: us
+fprBC = interp1(fprBC(2:size(fprBC),1),fprBC(2:size(fprBC),2),time,'pchip',0);
+
+%% Plot
+% Ka = 1000 nm^3.us-1
+figure(1)
+subplot(3,1,1) % Spatial Effects
+% x0=10;
+% y0=10;
+% width=400;
+% height=590;
+% set(gcf,'units','points','position',[x0,y0,width,height])
+hold on
+title('Spatial Effects');
+set(gca, 'xscale', 'log', 'fontsize',12, 'fontweight','bold');
+axis([1e-3 1e2 0 2000]);
+g1 = semilogx(time,theory, '-','Color',[0 0 0], 'LineWidth', 4);
+g2 = semilogx(time,det1D, '-.','Color',[0 0 1], 'LineWidth', 4);
+g3 = semilogx(time,det3D, '--','Color',[0 .75 1], 'LineWidth', 4);
+
+subplot(3,1,2) % Stochastic Effects
+hold on
+title('Stochastic Effects');
+set(gca, 'xscale', 'log', 'fontsize',12, 'fontweight','bold');
+axis([1e-3 1e2 0 2000]);
+ylabel('N_A(t)');
+g4 = semilogx(time,theory, '-','Color',[0 0 0], 'LineWidth', 4);
+g5 = semilogx(time,gillespie, '-.','Color',[0 .8 0], 'LineWidth', 3);
+g6 = semilogx(time,det1D, '--','Color',[0 0 1], 'LineWidth', 3);
+
+subplot(3,1,3) % Single Particle Methods
+hold on
+title('Single Particle Methods');
+set(gca, 'xscale', 'log', 'fontsize',12, 'fontweight','bold');
+axis([1e-3 1e2 0 2000]);
+xlabel('Time (us)');
+g7 = semilogx(time,theory, '-','Color',[0 0 0], 'LineWidth', 4);
+g8 = semilogx(time,mcell, '--','Color',[.5 0 .8], 'LineWidth', 3);
+g9 = semilogx(time,smoldyn, '-.','Color',[1 .5 0], 'LineWidth', 3);
+g10 = semilogx(time,fpr, '--','Color',[1 .85 0], 'LineWidth', 3);
+
+lgnd = legend([g1 g6 g3 g5 g8 g9 g10],'Theory','ODE','PDE','Gillespie',...
+    'MCell','Smoldyn','FPR');
+lgnd.FontSize = 11.5;
+lgnd.Position = [.02 .475 1 1];
+lgnd.Orientation = 'horizontal';
+
+% Absorbing BC
+figure(2)
+subplot(3,1,1) % Spatial Effects
+hold on
+title('Spatial Effects');
+set(gca, 'xscale', 'log', 'fontsize',12, 'fontweight','bold');
+axis([1e-3 1e2 0 2000]);
+g1b = semilogx(time,theoryBC, '-','Color',[0 0 0], 'LineWidth', 4);
+g2b = semilogx(time,det1DBC, '-.','Color',[0 0 1], 'LineWidth', 3);
+g3b = semilogx(time,det3DBC, '--','Color',[0 .75 1], 'LineWidth', 3);
+
+subplot(3,1,2) % Stochastic Effects
+hold on
+title('Stochastic Effects');
+set(gca, 'xscale', 'log', 'fontsize',12, 'fontweight','bold');
+axis([1e-3 1e2 0 2000]);
+ylabel('N_A(t)');
+g4b = semilogx(time,theoryBC, '-','Color',[0 0 0], 'LineWidth', 4);
+g5b = semilogx(time,gillespieBC, '-.','Color',[0 .8 0], 'LineWidth', 3);
+g6b = semilogx(time,det1DBC, '--','Color',[0 0 1], 'LineWidth', 3);
+
+subplot(3,1,3) % single Particle Methods
+hold on
+title('Single Particle Methods');
+set(gca, 'xscale', 'log', 'fontsize',12, 'fontweight','bold');
+axis([1e-3 1e2 0 2000]);
+xlabel('Time (us)');
+g7b = semilogx(time,theoryBC, '-','Color',[0 0 0], 'LineWidth', 4);
+g8b = semilogx(time,mcellBC, '--','Color',[.5 0 .8], 'LineWidth', 3);
+g9b = semilogx(time,smoldynBC, '--','Color',[1 .5 0], 'LineWidth', 3);
+g10b = semilogx(time,fprBC, '--','Color',[1 .85 0], 'LineWidth', 3);
+lgnd = legend([g1b g6b g3b g5b g8b g9b g10b],'Theory','ODE','PDE','Gillespie',...
+    'MCell','Smoldyn','FPR');
+lgnd.FontSize = 11.5;
+lgnd.Position = [.02 .475 1 1];
+lgnd.Orientation = 'horizontal';
+
+% Legend 
+%   Theory                  Black           [0 0 0]
+%   ODE Deterministic       Dark Blue       [0 0 1]
+%   PDE Deterministic       Light Blue      [0 .75 1]
+%   Gillespie               Green           [0 .8 0]
+%   FPR                     Yellow          [1 .85 0]
+%   Smoldyn                 Orange          [1 .5 0]
+%   MCell                   Purple          [.5 0 .8]
+%% Clear
+clear
+clc
