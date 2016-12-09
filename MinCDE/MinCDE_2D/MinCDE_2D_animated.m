4ae4a2fc17aad094e8b4d9b1c9c268685318397e
Athena Chen <achen70@jhu.edu>
2016-12-09 00:54:26 -0500

    MinCDE height and wavelength plots

---
+++ MinCDE/MinCDE_2D/MinCDE_2D_animated.m
@@ -0,0 +1,30 @@
+%% MinCDE2D Animated Line
+
+%% Import
+load('minDt.mat');
+load('EminDT.mat');
+load('time.mat');
+load('distance.mat');
+
+%% Animation
+figure(4)
+clf
+MinDt = animatedline('Color',[0 .75 1],'LineWidth',3);
+MinEDt = animatedline('Color',[0 0 1],'LineWidth',3);
+
+axis([0 6 0 450])
+xlabel('Distance along long axis (um)','FontSize',14);
+ylabel('N(x)','FontSize',14);
+title('Changes in Number of Particles over Time in MinCDE2D','FontSize',16);
+l=legend('MinDt','EminDT');
+l.FontSize = 14;
+for k = 1:length(time)*6
+    clearpoints(MinDt)
+    clearpoints(MinEDt)
+    
+    index = uint8(ceil(k/6));
+    addpoints(MinDt,distance,minDt(index,:))
+    addpoints(MinEDt,distance,EminDT(index,:))
+    
+    drawnow
+end
